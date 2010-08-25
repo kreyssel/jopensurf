18c4d463dbfb04c2d33717abc3e84a2de23cf3dd
jojopotato@gmail.com <jojopotato@gmail.com@714e5f76-d679-94a1-201b-066bf2ce06a2>
2010-08-25 07:06:20 +0000

    Moving the Clusterable interface to the K-Means library where it probably belongs (for now) and adding K-Means as a dependency on the JOpenSurf (not ideal)
    git-svn-id: http://jopensurf.googlecode.com/svn/trunk@16 714e5f76-d679-94a1-201b-066bf2ce06a2

--- src/com/stromberglabs/jopensurf/InterestPoint.java
+++ src/com/stromberglabs/jopensurf/InterestPoint.java
@@ -1,5 +1,7 @@
 package com.stromberglabs.jopensurf;
 
+import com.stromberglabs.cluster.Clusterable;
+
 public interface InterestPoint extends Clusterable {
 	public double getDistance(InterestPoint point);
 }
